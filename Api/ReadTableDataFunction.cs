e76ec58eff7bb9d0258ed7e5e931337cbdba44dd
abond325 <apb325@gmail.com>
2022-12-09 18:11:25 +0000

    Deploy functions

--- Api/ReadTableDataFunction.cs
+++ Api/ReadTableDataFunction.cs
@@ -18,17 +18,6 @@ namespace BlazorApp.Api
     public class ReadTableStorage
     {
         [FunctionName("ReadTableData")]
-        //public static Pageable<MyPoco> ReadTableData([HttpTrigger] dynamic input, ILogger log)
-        //{
-        //    var conStr = "DefaultEndpointsProtocol=https;AccountName=staticwebappstore;AccountKey=SABE2yIUUV1ENEjgN6tUPGOie8mSDBxTAimySZcYsiXoQhk9Wze6ddLF1YUCAlbkPGqtkE4iGMW/+AStMCzDyA==;EndpointSuffix=core.windows.net";
-
-        //    TableClient tableClient = new TableClient(conStr, "MyTable");
-        //    var entities = tableClient.Query<MyPoco>(x => x.PartitionKey == "Http");
-
-        //    return entities;
-
-        //}
-
         public static IActionResult ReadTableData(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
